2e8c787184d85da9ef464de8383155154af00485
Gaurav <gauravpatel3521@gmail.com>
2023-07-15 06:20:51 +0530

    f

--- app/vite.config.js
+++ app/vite.config.js
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+import react from '@vitejs/plugin-react'
+import { defineConfig, loadEnv } from 'vite';
+
+export default defineConfig(({ command, mode }) => {
+  
+    const env = loadEnv(mode, process.cwd(), '');
+    return {
+        define: {
+            'process.env.API_KEYY=': JSON.stringify(env.API_KEYY),
+            'process.env.YOUR_BOOLEAN_VARIABLE': env.YOUR_BOOLEAN_VARIABLE,
+            // If you want to exposes all env variables, which is not recommended
+            // 'process.env': env
+        },plugins: [react()]
+    };
+});
